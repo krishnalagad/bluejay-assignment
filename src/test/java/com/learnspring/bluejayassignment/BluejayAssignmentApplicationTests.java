71ccdf690d361afbb4605a8676ba5b898d8b2cd0
Krishna Lagad <krishnalagad2@gmail.com>
2023-10-06 11:20:08 +0530

    - dump project

---
+++ src/test/java/com/learnspring/bluejayassignment/BluejayAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.bluejayassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluejayAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
